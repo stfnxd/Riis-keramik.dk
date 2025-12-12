f3ed0a941fed0b9619fa9ef8b85cc5ec582fc990
stfn <riis@stfnxd.dk>
2025-12-13 00:02:24 +0100

    idk, sidste forsøg inden seng

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,19 +9,16 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        // Override the public path on production
+        // Only override public path on production
         if ($this->app->environment('production')) {
             $this->app->bind('path.public', function () {
-                return base_path('public_html');
+                return base_path('../public_html'); 
             });
         }
     }
 
     public function boot(): void
     {
-        $vite = $this->app->make(Vite::class);
 
-        // Use the build directory as usual (relative to public_path)
-        $vite->useBuildDirectory('build');
     }
 }
