d6546a973298afe637a06f319e9bae58b38afc48
stfn <riis@stfnxd.dk>
2025-12-13 00:10:09 +0100

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+            // optional, you can override manifest path
+            // to match your server build location
+            buildDirectory: process.env.APP_PUBLIC_PATH ? path.join(process.env.APP_PUBLIC_PATH, 'build') : 'public/build',
+        }),
+    ],
+    build: {
+        outDir: process.env.APP_PUBLIC_PATH ? path.join(process.env.APP_PUBLIC_PATH, 'build') : 'public/build',
+        emptyOutDir: true,
+    },
 });
