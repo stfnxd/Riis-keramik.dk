f131969ae9fb4f3ceb62ee01513b43110ebf779e
stfn <riis@stfnxd.dk>
2025-12-13 11:33:58 +0100

    add: accessPanel

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
 class User extends Authenticatable
 {
@@ -38,11 +40,19 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+
+    
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+        public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // midlertidigt til test
     }
 }
