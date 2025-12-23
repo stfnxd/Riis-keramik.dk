6c36af07804212bcfa5b52244733951580aadae8
stfn <riis@stfnxd.dk>
2025-12-23 12:46:18 +0100

    updated migrations

--- database/migrations/2025_09_25_144418_create_products_table.php
+++ database/migrations/2025_09_25_144418_create_products_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
         $table->boolean('featured')->default(false);
         $table->enum('availability_status', ['in_stock', 'made_to_order', 'sold_out'])
               ->default('in_stock');
-        $table->string('image_path')->nullable();
+        $table->json('image_path')->nullable()->change();
         $table->timestamps();
     });
 
