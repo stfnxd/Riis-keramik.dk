6b25abae195ac4e20b76c9fa9660fb1771df2081
stfn <riis@stfnxd.dk>
2025-12-25 21:14:47 +0100

    Added rouding in gallery

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -17,7 +17,7 @@
     </div>
 </div>
 
-<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 px-2 md:px-0">
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 px-2 md:px-0 rounded-2xl">
     @foreach($products as $product)
         <a href="{{ route('shop.show', $product) }}" class="block group">
             @if($product->image_url)
